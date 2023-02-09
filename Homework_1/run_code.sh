95a45e4ff335fbdc0bfb52610d8a506693e6c71c
Isaac Dahle <isaacbd.personal@gmail.com>
2023-02-09 00:24:23 -0800

    Ran the code on a small set of test data and confirmed that the results were correct

--- Homework_1/run_code.sh
+++ Homework_1/run_code.sh
@@ -2,38 +2,38 @@ import pandas as pd
 import numpy as np
 import itertools
 
-support = 100
+support = 2
 
 #* importing the file
 #pd.read_table('./test.txt', sep="  ", header=None)
 # reads the file data into a table with 38 rows
-rawFrame = pd.read_table('/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt', sep = ' ', header = None, names = range(38))
+#rawFrame = pd.read_table('/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt', sep = ' ', header = None, names = range(38))
 
 
+rawFrame = pd.read_table('test_data.txt', sep = ' ', header = None, names = range(38))
 # rawFrame = rawFrame.transpose()
 # print(rawFrame)
 #* finding frequent items
-
-# column_names = ["Freq"]
-# numFrame = rawFrame.apply(pd.Series.value_counts).transpose()
-# numFrame = numFrame.sum()
-# numFrame = pd.DataFrame(numFrame, columns = column_names)
-# print(numFrame)
-# freqItems = numFrame[numFrame['Freq'] > 99]
-# print(freqItems)
-
-
+# for row, key in rawFrame.iterrows():
+#     #print (row)
+    
+#     if not (key[1] == np.nan):  
+#         #if an item is present, add one to the frequency
+#         #else create the item in the dictionary and add 1
+#         freq_items[key[1]] = freq_items.get(key[1], 0) + 1
 
 #notice : python dictionaries are hashtables
 freq_items = dict()
 
-for row, key in rawFrame.iterrows():
-    #print (row)
-    
-    if not (key[1] == np.nan):  
-        #if an item is present, add one to the frequency
-        #else create the item in the dictionary and add 1
-        freq_items[key[1]] = freq_items.get(key[1], 0) + 1
+#generate a list of all items with their frequencies
+with open("test_data.txt") as file:
+    line_ids = list()
+    for line in file:
+        line_ids = line.split()
+
+        for item in line_ids:
+            freq_items[item] = freq_items.get(item, 0) + 1
+
 
 #create a list of keys to delete
 keys_to_del = [key for key in freq_items if freq_items[key] < support]
@@ -44,44 +44,52 @@ for key in keys_to_del:
 
 print("freq_items", freq_items)
 print("freq_itemstype", type(freq_items),len(freq_items))
+
+
+
+
 # #* Getting the canidate pairs   
 # # #a list of the combinations of frequent items. These combinations are tuples
 
-canidate_pairs = list(itertools.combinations(freq_items, 2))
+canidate_pairs = list(itertools.combinations(freq_items.keys(), 2))
+
+# creates a dictionary from canidate pairs
 freq_pairs =  {key: None for key in canidate_pairs}
+#initializes all the values to 0
 freq_pairs = dict.fromkeys(freq_pairs, 0)
-#print(freq_pairs)
+
 
 print("canidate_pairstype",(type(canidate_pairs)))
-print(("canidate_pairs",canidate_pairs[0:3]))
+print(("canidate_pairs",canidate_pairs))
 print("len",len(canidate_pairs))
-# # print(rawFrame.notna().mean())
-# # #print(canidate_pairs)
-# # #print(rawFrame)
-# #print(rawFrame)
-# #print(rawFrame.notna().mean())
-
-#for pair in canidate_pairs().key
 
 
-# #* remove the non frequent items from the data 
-# for row, col in rawFrame.iterrows():
-#     if col[1] not in freq_items.keys():
-#         col[1] = np.nan
 
-with open("/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt") as file:
+
+#with open("/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt") as file:
+
+print("freq_paris b4 weeding", freq_pairs)
+
+with open("test_data.txt") as file:
+
+    line_ids = list()
+
     for line in file:
-        ids = line.split()
+        line_ids = line.split()
         #print(ids)
-        line_pairs = list(itertools.combinations(ids, 2))  
+        line_pairs = list(itertools.permutations(line_ids, 2))  
+        print(line_pairs)
 
         for item in line_pairs:
             if (item in freq_pairs):
                 freq_pairs[item] += 1
+                print("added 1")
 
 
 #create a list of keys to delete
 keys_to_del = [key for key in freq_pairs if freq_pairs[key] < support]
+print("keys to delete", keys_to_del)
+
 
 #delete those items
 for key in keys_to_del:
