350e1be0c8e995ac186c0d8db915c72ead618124
Isaac Dahle <isaacbd.personal@gmail.com>
2023-02-09 00:39:59 -0800

    Used the tested code on the full dataset.

--- Homework_1/run_code.sh
+++ Homework_1/run_code.sh
@@ -2,15 +2,16 @@ import pandas as pd
 import numpy as np
 import itertools
 
-support = 2
+support = 100
 
 #* importing the file
 #pd.read_table('./test.txt', sep="  ", header=None)
 # reads the file data into a table with 38 rows
+
+
+
 #rawFrame = pd.read_table('/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt', sep = ' ', header = None, names = range(38))
-
-
-rawFrame = pd.read_table('test_data.txt', sep = ' ', header = None, names = range(38))
+#rawFrame = pd.read_table('test_data.txt', sep = ' ', header = None, names = range(38))
 # rawFrame = rawFrame.transpose()
 # print(rawFrame)
 #* finding frequent items
@@ -26,7 +27,9 @@ rawFrame = pd.read_table('test_data.txt', sep = ' ', header = None, names = rang
 freq_items = dict()
 
 #generate a list of all items with their frequencies
-with open("test_data.txt") as file:
+
+with open("/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt") as file:
+#with open("test_data.txt") as file:
     line_ids = list()
     for line in file:
         line_ids = line.split()
@@ -42,15 +45,16 @@ keys_to_del = [key for key in freq_items if freq_items[key] < support]
 for key in keys_to_del:
     del freq_items[key]
 
-print("freq_items", freq_items)
-print("freq_itemstype", type(freq_items),len(freq_items))
+#print("freq_items", freq_items)
+#print("freq_itemstype", type(freq_items),len(freq_items))
 
 
 
 
 # #* Getting the canidate pairs   
 # # #a list of the combinations of frequent items. These combinations are tuples
-
+print("canidate pairs")
+#"
 canidate_pairs = list(itertools.combinations(freq_items.keys(), 2))
 
 # creates a dictionary from canidate pairs
@@ -59,18 +63,23 @@ freq_pairs =  {key: None for key in canidate_pairs}
 freq_pairs = dict.fromkeys(freq_pairs, 0)
 
 
-print("canidate_pairstype",(type(canidate_pairs)))
-print(("canidate_pairs",canidate_pairs))
-print("len",len(canidate_pairs))
+# print("canidate_pairstype",(type(canidate_pairs)))
+# print(("canidate_pairs",canidate_pairs))
+# print("len",len(canidate_pairs))
+
+#* delete non frequent items
 
 
 
 
-#with open("/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt") as file:
 
-print("freq_paris b4 weeding", freq_pairs)
 
-with open("test_data.txt") as file:
+#* Getting the freqent pairs
+#print("freq_paris b4 weeding", freq_pairs)
+print("begin freq pairs")
+#"
+with open("/home/isaac/Repos/Data_Mining/Homework_1/browsing-data.txt") as file:
+#with open("test_data.txt") as file:
 
     line_ids = list()
 
@@ -78,17 +87,17 @@ with open("test_data.txt") as file:
         line_ids = line.split()
         #print(ids)
         line_pairs = list(itertools.permutations(line_ids, 2))  
-        print(line_pairs)
+        #print(line_pairs)
 
         for item in line_pairs:
             if (item in freq_pairs):
                 freq_pairs[item] += 1
-                print("added 1")
+                #print("added 1")
 
-
+#"
 #create a list of keys to delete
 keys_to_del = [key for key in freq_pairs if freq_pairs[key] < support]
-print("keys to delete", keys_to_del)
+#print("keys to delete", keys_to_del)
 
 
 #delete those items
